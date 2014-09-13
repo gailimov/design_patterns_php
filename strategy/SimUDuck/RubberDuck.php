e21ba21fa3c743c434c70b7984f3c1ff67dc264c
Kanat Gailimov <gailimov@gmail.com>
2014-09-14 02:34:19 +0600

    Added rubber duck

---
+++ strategy/SimUDuck/RubberDuck.php
@@ -0,0 +1,14 @@
+<?php
+
+class RubberDuck extends Duck
+{
+    public function quack()
+    {
+        return 'squeak';
+    }
+
+    public function display()
+    {
+        return 'displaying rubber duck';
+    }
+}
--- strategy/SimUDuck/init.php
+++ strategy/SimUDuck/init.php
@@ -3,6 +3,7 @@
 require_once 'Duck.php';
 require_once 'MallardDuck.php';
 require_once 'RedheadDuck.php';
+require_once 'RubberDuck.php';
 
 echo 'Mallard duck:' . PHP_EOL;
 $mallard = new MallardDuck();
@@ -19,3 +20,12 @@ echo $redhead->quack() . PHP_EOL;
 echo $redhead->swim() . PHP_EOL;
 echo $redhead->display() . PHP_EOL;
 echo $redhead->fly() . PHP_EOL;
+
+echo '=========================' . PHP_EOL;
+
+echo 'Rubber duck:' . PHP_EOL;
+$rubber = new RubberDuck();
+echo $rubber->quack() . PHP_EOL;
+echo $rubber->swim() . PHP_EOL;
+echo $rubber->display() . PHP_EOL;
+echo $rubber->fly() . PHP_EOL;
